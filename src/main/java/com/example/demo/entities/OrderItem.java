f5392a95a95c06925a4cd3b1ca3faf1809c13464
MrNish <91740871+MrNish@users.noreply.github.com>
2025-06-29 23:58:26 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/entities/OrderItem.java
@@ -0,0 +1,109 @@
+package com.example.demo.entities;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	@JoinColumn(name = "product_id")
+	private int productId;
+	
+	@Column
+	private int quantity;
+	
+	@Column
+	private BigDecimal pricePerUnit;
+	
+	@Column
+	private BigDecimal totalPrice;
+
+	public OrderItem() {
+		super();
+	}
+
+	public OrderItem(Order order, int productId, int quantity, BigDecimal pricePerUnit, BigDecimal totalPrice) {
+		super();
+		this.order = order;
+		this.productId = productId;
+		this.quantity = quantity;
+		this.pricePerUnit = pricePerUnit;
+		this.totalPrice = totalPrice;
+	}
+
+	public OrderItem(int id, Order order, int productId, int quantity, BigDecimal pricePerUnit, BigDecimal totalPrice) {
+		super();
+		this.id = id;
+		this.order = order;
+		this.productId = productId;
+		this.quantity = quantity;
+		this.pricePerUnit = pricePerUnit;
+		this.totalPrice = totalPrice;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getPricePerUnit() {
+		return pricePerUnit;
+	}
+
+	public void setPricePerUnit(BigDecimal pricePerUnit) {
+		this.pricePerUnit = pricePerUnit;
+	}
+
+	public BigDecimal getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(BigDecimal totalPrice) {
+		this.totalPrice = totalPrice;
+	}	
+}
